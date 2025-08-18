1265ac5074a7299e2e9925ba19acf0164439e355
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-08-18 15:05:54 +0500

    Time: 1090 ms (16.57%), Space: 17.8 MB (88.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,30 +1,11 @@
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-
-        # Frequency arrays for s1 and sliding window in s2
-        s1_count = [0] * 26
-        s2_count = [0] * 26
-
-        for ch in s1:
-            s1_count[ord(ch) - ord('a')] += 1
-
-        # First window
-        for ch in s2[:len(s1)]:
-            s2_count[ord(ch) - ord('a')] += 1
-
-        if s1_count == s2_count:
-            return True
-
-        # Sliding window
-        for i in range(len(s1), len(s2)):
-            # Add new char
-            s2_count[ord(s2[i]) - ord('a')] += 1
-            # Remove old char
-            s2_count[ord(s2[i - len(s1)]) - ord('a')] -= 1
-
-            if s1_count == s2_count:
+        s1Count = Counter(s1)
+        left = 0
+        for right in range(len(s1),len(s2)+1):
+            if s1Count == Counter(s2[left:right]):
                 return True
-
+            else:
+                left +=1
         return False
