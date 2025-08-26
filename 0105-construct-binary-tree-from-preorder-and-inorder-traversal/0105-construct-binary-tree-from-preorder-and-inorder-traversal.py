5f62569b7ba4feb948b51286877b08a4e72ce44f
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-08-26 19:14:28 +0500

    Time: 98 ms (27.43%), Space: 89.3 MB (35.55%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,27 +6,11 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        # Map value -> index in inorder for O(1) lookup
-        inorder_index = {val: idx for idx, val in enumerate(inorder)}
-        
-        # Use an index to track root position in preorder
-        self.pre_idx = 0
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid+1])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:]) 
 
-        def array_to_tree(left, right):
-            # if no elements left in this subtree
-            if left > right:
-                return None
-
-            # pick current root from preorder
-            root_val = preorder[self.pre_idx]
-            self.pre_idx += 1
-            root = TreeNode(root_val)
-
-            # build left & right subtrees using inorder index
-            root.left = array_to_tree(left, inorder_index[root_val] - 1)
-            root.right = array_to_tree(inorder_index[root_val] + 1, right)
-
-            return root
-
-        return array_to_tree(0, len(inorder) - 1)
-        
+        return root       
