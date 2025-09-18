72951419e7f072696bab6c2d27d9d00815fe4cb6
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-09-18 22:42:58 +0500

    Time: 8 ms (84.88%), Space: 18.1 MB (82.71%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -2,36 +2,29 @@ from typing import List
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [["."] * n for _ in range(n)]
+        cols = set()
+        posDiag = set()
+        negDiag = set()
+
+        board = [["."] * n for i in range(n)]
         res = []
-        
-        cols = set()      
-        posDiag = set()   
-        negDiag = set()   
-
-        def backtrack(r):
-            if r == n:  
-                res.append(["".join(row) for row in board])
-                return
-            
+        def backTrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return 
             for c in range(n):
-                if c in cols or (r + c) in posDiag or (r - c) in negDiag:
-                    continue 
-                
-              
-                board[r][c] = "Q"
+                if c in cols or (r-c) in negDiag or (r+c) in posDiag:
+                    continue
                 cols.add(c)
-                posDiag.add(r + c)
-                negDiag.add(r - c)
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+                board[r][c] = "Q"
+                backTrack(r+1)
 
-             
-                backtrack(r + 1)
-
-               
-                board[r][c] = "."
                 cols.remove(c)
-                posDiag.remove(r + c)
-                negDiag.remove(r - c)
-
-        backtrack(0)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+                board[r][c] = "."
+        backTrack(0)
         return res
