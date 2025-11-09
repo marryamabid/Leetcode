0cab97d272276ea8814d0514e2d0676a6962050c
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-11-09 23:53:15 +0500

    Time: 95 ms (38.22%), Space: 89.4 MB (20.31%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,13 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from typing import List, Optional
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
             return None
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid+1])
-        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:]) 
+        
+        # The first element in preorder is the root
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+        
+        # Find the index of root in inorder list
+        root_index = inorder.index(root_val)
+        
+        # Recursively build left and right subtrees
+        # Left subtree: elements before root_index in inorder
+        # Right subtree: elements after root_index in inorder
+        root.left = self.buildTree(preorder[1:1+root_index], inorder[:root_index])
+        root.right = self.buildTree(preorder[1+root_index:], inorder[root_index+1:])
+        
+        return root
 
-        return root       
+        
