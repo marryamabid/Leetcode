f810383435298fc03461659bccf2b632750b13cc
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-11-14 15:14:29 +0500

    Time: 0 ms (100%), Space: 17.7 MB (83.05%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0  # pointer for next position to insert non-val
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+        
