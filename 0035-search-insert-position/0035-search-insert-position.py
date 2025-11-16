e4f6c3d1028681140506bae09ab184d92d9c9c80
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-11-16 22:37:38 +0500

    Time: 0 ms (100%), Space: 18.4 MB (72.63%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        low, high = 0, len(nums) - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        # If not found, low is the correct insert position
+        return low
+
+        
