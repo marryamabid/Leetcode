e1cd98b26b2b8e18af1f72344984ea296c63fc10
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-11-30 00:04:12 +0500

    Time: 3 ms (87.41%), Space: 19.1 MB (84.28%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,26 +5,30 @@
 #         self.left = left
 #         self.right = right
 
-from typing import List, Optional
-
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        
-        # The first element in preorder is the root
-        root_val = preorder[0]
-        root = TreeNode(root_val)
-        
-        # Find the index of root in inorder list
-        root_index = inorder.index(root_val)
-        
-        # Recursively build left and right subtrees
-        # Left subtree: elements before root_index in inorder
-        # Right subtree: elements after root_index in inorder
-        root.left = self.buildTree(preorder[1:1+root_index], inorder[:root_index])
-        root.right = self.buildTree(preorder[1+root_index:], inorder[root_index+1:])
-        
-        return root
+
+        # Map value -> index in inorder for O(1) lookup
+        index_map = {val: i for i, val in enumerate(inorder)}
+        self.pre_idx = 0
+
+        def helper(left, right):
+            if left > right:
+                return None
+
+            # Pick root from preorder
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+
+            root = TreeNode(root_val)
+
+            # Build left and right subtrees using inorder boundaries
+            mid = index_map[root_val]
+            root.left = helper(left, mid - 1)
+            root.right = helper(mid + 1, right)
+
+            return root
+
+        return helper(0, len(inorder) - 1)
 
         
