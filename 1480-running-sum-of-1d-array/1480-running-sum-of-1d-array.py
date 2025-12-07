cc2772fe3c500c11575da1dcb98cf2ed3a5d6348
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-12-07 19:19:06 +0500

    Time: 0 ms (100%), Space: 18.1 MB (24.29%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        cur=0
+        for i,num in enumerate(nums):
+            cur += num
+            nums[i] = cur
+        return nums
