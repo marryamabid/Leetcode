8d46e8894912c60349f3731dac8507fb34ebf6ed
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-12-15 21:30:53 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -1,57 +1,19 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.end = False
-
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word):
-        node = self.root
-        for ch in word:
-            if ch not in node.children:
-                node.children[ch] = TrieNode()
-            node = node.children[ch]
-        node.end = True
-
 
 class Solution:
     def findAllConcatenatedWordsInADict(self, words):
-        trie = Trie()
-        words.sort(key=len) 
-        result = []
+        wordSet = set(words)
 
-        def canForm(word, start, count, memo):
-            if start == len(word):
-                return count >= 2
-
-            if start in memo:
-                return memo[start]
-
-            node = trie.root
-            for i in range(start, len(word)):
-                ch = word[i]
-                if ch not in node.children:
-                    break
-                node = node.children[ch]
-
-                if node.end:
-                    if canForm(word, i + 1, count + 1, memo):
-                        memo[start] = True
-                        return True
-
-            memo[start] = False
+        def dfs(word):
+            for i in range(1,len(word)):
+                prefix = word[:i]
+                suffix = word[i:]
+                if ((prefix in wordSet and suffix in wordSet) or
+                    prefix in wordSet and dfs(suffix)):
+                    return True
             return False
 
+        res = []
         for w in words:
-            if not w:
-                continue
-            memo = {}
-            if canForm(w, 0, 0, memo):
-                result.append(w)
-            else:
-                trie.insert(w)
-
-        return result
+            if dfs(w):
+                res.append(w)
+        return res
