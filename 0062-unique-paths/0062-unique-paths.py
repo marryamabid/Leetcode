ae5c06a5ab1b7ce78162da2d77c73bcd65354313
marryamabid <162056299+marryamabid@users.noreply.github.com>
2025-12-22 22:13:43 +0500

    Time: 0 ms (100%), Space: 17.1 MB (99.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,12 +1,9 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int, map={}) -> int:
-        key = str(m) +","+ str(n)
-        if (key in map):
-            return map[key]
-        if (m==1 and n==1):
-            return 1
-        if (m==0 or n== 0 ):
-            return 0
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+        return dp[-1]
+
         
-        map[key] = self.uniquePaths(m-1,n,map) + self.uniquePaths(m,n-1,map) 
-        return map[key]
