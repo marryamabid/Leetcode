0bf6fafa553eb985421d221745ddf5f93696c80b
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-01-17 09:16:26 +0500

    Time: 3 ms (50.89%), Space: 63.1 MB (66.97%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let maxArea = 0
+    let left = 0
+    let right = height.length-1
+    while(left<right){
+        let h = Math.min(height[left],height[right])
+        let w = right - left
+        let area = h*w
+        maxArea = Math.max(maxArea,area)
+        if(height[left] < height[right]){
+            left++
+        }else{
+            right--
+        }
+    }
+    return maxArea
+};
