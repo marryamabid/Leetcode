7ce8be830bd761bfb7757872e8ac083ac53df481
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-01-31 22:51:41 +0500

    Time: 12 ms (92.4%), Space: 73.7 MB (69.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,14 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums){
-    const map ={}
-    for(let i=0; i < nums.length;i++){
-        map[nums[i]] = ( map[nums[i]] || 0) +1
-    }
-    for(let int of nums){
-        if(map[int] > 1){
-            return true
-        }
+    const set = new Set()
+    for(let i=0 ; i< nums.length; i++){
+        if(set.has(nums[i])) return true
+        set.add(nums[i]) 
     }
     return false
 }  
