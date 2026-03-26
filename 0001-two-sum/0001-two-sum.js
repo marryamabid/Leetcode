136dce9da4cf75210e58cae7ee685b639ac0e226
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-03-26 22:36:44 +0500

    Time: 2 ms (68.19%), Space: 55.9 MB (22.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   let map = new Map()
-   for(let i=0; i < nums.length; i++){
-        diff =  target - nums[i]
-        if(map.has(diff)){
-            return [map.get(diff),i]
+   let newMap = new Map()
+   for(let i=0 ; i < nums.length; i++){
+        let diff = target - nums[i]
+        if(newMap.has(diff)){
+            return [newMap.get(diff),i]
         }
-        map.set(nums[i],i) 
+        newMap.set(nums[i],i) 
    }
 };
