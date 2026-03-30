55a546bcaac18b33f709975b535b8f220c6ce425
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-03-30 19:39:17 +0500

    Time: 0 ms (100%), Space: 54.4 MB (22.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -5,7 +5,7 @@
  */
 var search = function(nums, target) {
     for(let i=0; i < nums.length; i++){
-        if(nums[i] === target){
+        if(nums[i] == target){
             return i
         }
     }
