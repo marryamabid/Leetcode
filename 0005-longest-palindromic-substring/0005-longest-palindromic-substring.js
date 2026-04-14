04bbcf4a8dfa9f06bf751338cab2b664f6773286
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-04-14 21:46:08 +0500

    Time: 18 ms (82.5%), Space: 56.4 MB (51.09%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let res = ""
+    let maxLen = 0
+
+    for(let i= 0; i < s.length; i++){
+        //for odd centre outward position
+        let l = i 
+        let r = i
+        while( l >= 0  && r< s.length && s[l] === s[r]){
+            if((r-l+1)>maxLen){
+                res = s.slice(l,r+1)
+                maxLen = r-l+1
+            }
+            l--
+            r++
+        }   
+    }
+    for(let i= 0; i < s.length; i++){
+        //for even centre outward position
+        let l = i 
+        let r = i+1
+        while( l >= 0  && r< s.length && s[l] === s[r]){
+            if((r-l+1)>maxLen){
+                res = s.slice(l,r+1)
+                maxLen = r-l+1
+            }
+            l--
+            r++
+        }   
+    
+}
+    return res
+}
