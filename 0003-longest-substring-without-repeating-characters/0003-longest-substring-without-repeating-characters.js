355e613c1dff260ca2fcd6f786f83ae11bc48557
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-04-14 21:01:51 +0500

    Time: 7 ms (55.47%), Space: 58 MB (60.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,16 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let charSet = new Set()
-    let left=0
-    let res = 0
-    for(let right = 0; right< s.length; right++){
-        while (charSet.has(s[right])){
+   let charSet = new Set()
+   let maxLen = 0
+   let left = 0
+   for(let right=0 ; right< s.length; right++){
+        while(charSet.has(s[right])){
             charSet.delete(s[left])
             left++
         }
         charSet.add(s[right])
-        res = Math.max(res,right-left+1)
-    }
-    return res
+        maxLen = Math.max(maxLen,right-left+1)
+
+   }
+   return maxLen
+
+
 };
