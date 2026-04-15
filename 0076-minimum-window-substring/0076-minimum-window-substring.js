4b263ddc934eb37759c5ddf661c272f04e86789f
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-04-15 21:52:08 +0500

    Time: 290 ms (7.55%), Space: 61.2 MB (18.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -4,39 +4,36 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-    if(t.length > s.length) return ""
-    let need ={}
-    for(let char of t){
-        need[char] = (need[char] || 0)+1
+    if(t.length > s.length) return""
+    let need = {}
+    for(let ch of t){
+        need[ch] = (need[ch] || 0 )+1
+
     }
-    let window = {}
     let left = 0
-    let right=0
-    let start =0
-    let valid =0
+    let right = 0
+    let window = {}
+    let have = 0
     let minLen = Infinity
     while(right < s.length){
         let c = s[right]
         right++
         if(need[c]){
-            window[c]=(window[c] || 0)+1
-            if (window[c] === need[c]) valid++;
+            window[c] = (window[c] || 0)+1
+            if(window[c] === need[c]) have++
         }
-    while(valid === Object.keys(need).length){
-        if(right-left < minLen){  
-            start = left
-            minLen = right-left //We store the best answer so far.
-        }
-        let d = s[left]
-        left++
-        if(need[d]){
-            if(window[d]=== need[d] ) valid--
-            window[d]--
+        while(have === Object.keys(need).length){
+            if(right-left < minLen){
+                start = left
+                minLen = right-left
+            }
+            let d = s[left]
+            left++
+            if(need[d]){
+                if(window[d] ===  need[d]) have--
+                window[d]--
+            }
         }
     }
-
-    }
-    
-    
-    return minLen === Infinity ? "" : s.substring(start, start + minLen);
+    return minLen === Infinity ?  "" : s.substring(start,start+minLen)
 };
