7477eeaa3b6acdf8a7facde3849244356890008b
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-04-16 21:21:18 +0500

    Time: 2 ms (48.28%), Space: 57.3 MB (60.38%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    let  i =0
+    let sign = 1
+    let  n = s.length
+    while(i<n && s[i] ===" "){
+        i++
+    }
+  
+    if (s[i] === '+' || s[i] === '-') {
+            if (s[i] === '-') sign = -1;
+            i++;
+    }
+
+    
+    let result =0
+    while(i<n && s[i] >="0" && s[i] <="9"){
+        let digit = s[i] - '0'
+       if (result > Math.floor((2**31 - 1) / 10) ||
+            (result === Math.floor((2**31 - 1) / 10) && digit > 7)) {
+            return sign === 1 ? 2**31 - 1 : -(2**31);
+        }
+
+        result = result * 10 + digit;
+        i++
+    }
+    return sign*result
+};
