7d230a5016d167c8ea487b2e6deab6a808146c03
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-04-17 19:02:22 +0500

    Time: 1 ms (88%), Space: 56.4 MB (76.66%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -3,29 +3,32 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    let  i =0
-    let sign = 1
-    let  n = s.length
+    let  i=0
+    let sign =1
+    let n = s.length
     while(i<n && s[i] ===" "){
         i++
     }
-  
-    if (s[i] === '+' || s[i] === '-') {
-            if (s[i] === '-') sign = -1;
-            i++;
-    }
-
-    
-    let result =0
-    while(i<n && s[i] >="0" && s[i] <="9"){
-        let digit = s[i] - '0'
-       if (result > Math.floor((2**31 - 1) / 10) ||
-            (result === Math.floor((2**31 - 1) / 10) && digit > 7)) {
-            return sign === 1 ? 2**31 - 1 : -(2**31);
+    if(s[i] ==="-" || s[i] === "+"){
+        if(s[i] ==="-"){
+            sign = -1
         }
-
-        result = result * 10 + digit;
         i++
     }
-    return sign*result
+    let result =0
+    while(i<n && (s[i] >= "0" && s[i] <="9")){
+        let integer = s[i]
+        let digit = integer -"0"
+        if(
+            result> Math.floor((2**31-1)/10)||
+            (result === Math.floor((2**31-1)/10) && digit>7)
+            ){
+            return sign===1 ? 2**31-1 : -(2**31)
+        }
+        result = result * 10 + digit
+       
+        i++
+    }
+    return sign * result
+
 };
