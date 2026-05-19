93d75a32914d757c002aee9bb77a95db05b8590f
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-05-19 16:59:06 +0500

    Time: 53 ms (30.69%), Space: 56.2 MB (86.44%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -18,35 +18,13 @@ var detectCycle = function(head) {
         slow = slow.next
         fast = fast.next.next
         if(slow === fast){
-            cycleLength = findCycleLen(slow)
-            break
+            let start = head
+            while(start !== slow){
+                start = start.next
+                slow = slow.next
+            }
+            return start
         }
     }
-    if (cycleLength === 0) return null;
-
-    return findStart(head, cycleLength);
+    return null
 };
-function findCycleLen(slow){
-    let cycleLen = 0
-    let current = slow
-    while(true){
-        current = current.next
-        cycleLen++
-        if(current === slow) break
-        
-    }
-    return cycleLen
-}
-function findStart(head,cycleLength){
-    let pointer1 = head
-    let pointer2 = head
-    while(cycleLength>0){
-        pointer2 = pointer2.next
-        cycleLength--
-    }
-    while(pointer1 !== pointer2){
-        pointer1 = pointer1.next
-        pointer2 = pointer2.next
-    }
-    return pointer1
-}
