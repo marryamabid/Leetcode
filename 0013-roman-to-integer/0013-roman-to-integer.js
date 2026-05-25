4f166b629c0d4eadc2cbd54a68d5c732804e7e46
marryamabid <162056299+marryamabid@users.noreply.github.com>
2026-05-25 23:17:05 +0500

    Time: 6 ms (41.56%), Space: 60.2 MB (93.44%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,15 +3,28 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const map = {I:1,V:5,X:10,L:50,C:100,D:500,M:1000};
-    let count = 0
-    for(let i=0 ; i< s.length; i+=1){
-        if(map[s[i]] < map[s[i+1]]){
-            count -= map[s[i]]
-        }
-        else{
-            count += map[s[i]]
+    const map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let current = map[s[i]];
+        let next = map[s[i + 1]];
+
+        if (next > current) {
+            total -= current;
+        } else {
+            total += current;
         }
     }
-    return count
+
+    return total;
 };
